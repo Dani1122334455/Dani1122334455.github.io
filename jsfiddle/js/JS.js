3be1b658be5c708d17703fd04472e6ea59a36200
DaniRomero <60660757+Dani1122334455@users.noreply.github.com>
2020-10-05 13:34:46 +0200

    jsfiddle

---
+++ jsfiddle/js/JS.js
@@ -0,0 +1,9 @@
+var contador = 0;
+function Saludar() {
+	var nombre = document.getElementById("texto").value;
+   document.getElementById("texto2").innerHTML = "Saludos a Mundo y a "+nombre;
+   contador++;
+   document.getElementById("saludos").innerHTML = "Se ha saludado: "+contador+" veces";
+   
+   
+}
